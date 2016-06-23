3a8bf8150a903fd80c43c95b11d1e5d59f3a84ee
charles <xiecharles@163.com>
2016-06-23 08:52:11 +0800

    1.pom.xml添加Struts2的jar包;2.新增可变参数测试类

---
+++ src/test/java/org/tkzc/aaabase/string/VarLenParamTest.java
@@ -0,0 +1,26 @@
+package org.tkzc.aaabase.string;
+
+public class VarLenParamTest {
+
+	public static void main(String[] args) {
+		VarLenParamTest vlpt = new VarLenParamTest();
+		vlpt.print(0);
+		vlpt.print(0, "hello", "world");
+		
+		vlpt.print1(0, new String[] {"hello", "world"});
+	}
+	
+	public void print(int ai, String... params) {
+		System.out.println(ai);
+		for (String string : params) {
+			System.out.println(string);
+		}
+	}
+	
+	public void print1(int ai, String[] params) {
+		System.out.println(ai);
+		for (String string : params) {
+			System.out.println(string);
+		}
+	}
+}
